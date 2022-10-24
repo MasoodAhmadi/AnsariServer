5c620278e95dc4cb39db0099f0569d22645a533f
Masood Ahmadi <masood@eduix.it>
2022-10-24 11:42:08 +0300

    api added

--- app.js
+++ app.js
@@ -1,42 +1,21 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-
-const usersRouter = require("./routes/users");
-
+const menuRouter = require('./controllers/menus');
+const citiesRouter = require('./controllers/cities');
+const choicesRouter = require('./controllers/foodChoices');
+const trainModelsRouter = require('./controllers/trainModels');
+const express = require('express');
+const cors = require('cors');
+const config = require('./utils/config');
+const middleware = require('./utils/middleware');
 const app = express();
 
-// view engine setup
-//app.set("views", path.join(__dirname, "views"));
-//app.set("view engine", "jade");
-
-app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/users", usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-app.listen(3009, function () {
-  console.log("Example app listening on port 3000!");
-});
+app.use(middleware.requestLogger);
+app.use('/api/menus', menuRouter);
+app.use('/api/cities', citiesRouter);
+app.use('/api/foodchoices', choicesRouter);
+app.use('/api/trainmodels', trainModelsRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
---
+++ data/cities.js
@@ -0,0 +1,29 @@
+let cities = [
+  {
+    'city': 'Helsinki',
+    'country': 'Finland',
+    'iso2': 'FI'
+  },
+  {
+    'city': 'Tampere',
+    'country': 'Finland',
+    'iso2': 'FI'
+  },
+  {
+    'city': 'Turku',
+    'country': 'Finland',
+    'iso2': 'FI'
+  },
+  {
+    'city': 'Seinäjoki',
+    'country': 'Finland',
+    'iso2': 'FI'
+  },
+  {
+    'city': 'Rovaniemi',
+    'country': 'Finland',
+    'iso2': 'FI'
+  }
+];
+
+module.exports = cities;
